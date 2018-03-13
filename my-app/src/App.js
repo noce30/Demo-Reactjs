c8889261ebfc095161a706bb2daf58397611a839
darrenpham <darren.pham@unicorn.vn>
2018-03-13 12:19:24 +0700

    -task: gender

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
+import icon_female from './icon_female.png';
 import './App.css';
+import Toggle from 'react-bootstrap-toggle';
+import Switch from 'react-bootstrap-switch';
 
 class App extends Component {
+  constructor() {
+    super();
+  }
+
+  handleSwitch(elem, state) {
+    console.log('handleSwitch. elem:', elem);
+    console.log('name:', elem.props.name);
+    console.log('new state:', state);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <form>
+        <div className="item">
+          <div className="right">
+            <img src="icon_female.png" alt="male" />
+            <label className="switch">
+              <input type="checkbox" defaultChecked />
+              <span className="slider round"></span>
+            </label>
+            <img src={icon_female} alt="male" />
+          </div>
+
+        </div>
+      </form>
+    )
   }
+
 }
 
 export default App;
