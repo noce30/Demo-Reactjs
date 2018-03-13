76374dce98866e4a689c544ada6e7ded75865df3
Sony Trinh <sony.trinh@unicorn.vn>
2018-03-13 17:03:22 +0700

    merge code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,39 +1,22 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import logo from './logo.svg';
 import './App.css';
 import PersonalInfoPanel from "./components/PersonalInfoPanel";
-import icon_female from './assets/img/icons-demo/icon_female.png';
-import male from './assets/img/icons-demo/icon_male.png';
-import Gender from './components/Gender';
+import CurveOptions from "./components/CurveOptions";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = { checked: true };
-    this.handleGender = this.handleGender.bind(this);
-  }
-
-  handleGender(checked) {
-    this.setState({
-      checked: checked
-    });
-  }
-
   render() {
     return (
       <div className="bg-image">
-      <div className="main-bg">
-        <PersonalInfoPanel />
-        <img src={this.state.checked ? icon_female : male} />
-          <Gender
-            onGenderChange={this.handleGender}
-            checked={this.state.checked}
-          />
-       </div>
+        <div className="main-bg">
+          <div className="info-position">
+            <PersonalInfoPanel />
+          </div>
+            <CurveOptions />
+        </div>
       </div>
     );
   }
-
 }
 
 
