780b82a104eae2321ea989c6b9d468796d89074c
Sony Trinh <sony.trinh@unicorn.vn>
2018-03-13 17:03:22 +0700

    Add curve

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import PersonalInfoPanel from "./components/PersonalInfoPanel";
+import CurveOptions from "./components/CurveOptions";
 
 class App extends Component {
   render() {
     return (
       <div className="bg-image">
-      <div className="main-bg">
-        <PersonalInfoPanel />
-       </div>
+        <div className="main-bg">
+          <div className="info-position">
+            <PersonalInfoPanel />
+          </div>
+            <CurveOptions />
+        </div>
       </div>
     );
   }
