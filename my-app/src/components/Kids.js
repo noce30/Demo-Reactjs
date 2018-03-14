7ba8f0f21b0fab869e60de8c799ee28552aa552b
Sony Trinh <sony.trinh@unicorn.vn>
2018-03-14 14:25:36 +0700

    update kids

--- my-app/src/components/Kids.js
+++ my-app/src/components/Kids.js
@@ -3,23 +3,29 @@ import iconKid from "../assets/img/icons-demo/icon_kids.png";
 
 const Kids = (props) => {
     const selectedNumber = props.selectedNumber;
-    if (!props.isShowSelectKidsOption) {
-        return <div>
-            <img src={iconKid} onClick={e => props.onClickToShowSelectNumber(true)} />
-            <span className="field-info">Kids</span>
+
+    return (
+        <div>
+            <div>
+                <img src={iconKid} onClick={e => props.onClickToShowSelectNumber(true)} />
+                <div>
+                    <span className="field-info">Kids</span>
+                </div>
+            </div>
+            {props.isShowSelectKidsOption &&
+                <div>
+                    <select name="kids" value={selectedNumber} onChange={e => {
+                        props.onSelectKidsNumber(e.target.value)
+                        props.onClickToShowSelectNumber(false)
+                    }}>
+                        {props.numberOfKidsOptions.items.map((opt, i) => {
+                            return <option key={i} value={opt}>{opt}</option>
+                        })}
+                    </select>
+                </div>
+            }
         </div>
-    }
-    return <div>
-        <select name="kids" value={selectedNumber} onChange={e => {
-            props.onSelectKidsNumber(e.target.value)
-            props.onClickToShowSelectNumber(false)
-        }}>
-            {props.numberOfKidsOptions.items.map((opt, i) => {
-                return <option key={i} value={opt}>{opt}</option>
-            })}
-        </select>
-        <span className="field-info">Select number of kids</span>
-    </div>
+    )
 }
 
 export default Kids;
