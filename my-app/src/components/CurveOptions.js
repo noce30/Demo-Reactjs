9f5223d88fe1ab0f2e65d80dee947368b3b6e841
darrenpham <darren.pham@unicorn.vn>
2018-03-16 15:47:38 +0700

    - remove gender toggle

--- my-app/src/components/CurveOptions.js
+++ my-app/src/components/CurveOptions.js
@@ -11,14 +11,15 @@ import { SLECT_SMOKING } from '../constants/actionConstants';
 import icon_smoking from '../assets/img/icons-demo/icon_smoking.png';
 import icon_female from '../assets/img/icons-demo/icon_female.png';
 import icon_male from '../assets/img/icons-demo/icon_male.png';
-import {SELECT_GENDER} from '../constants/actionConstants';
+import { SELECT_GENDER } from '../constants/actionConstants';
+import icon_select_gender from '../assets/img/icons-demo/icon_select_gender.png';
 
 class CurveOptions extends Component {
     render() {
         return (
             <div>
                 <ul className="circle-container">
-                    <li><Gender/></li>
+                    <li><Gender /></li>
                     <li><MaritalStatusContainer /></li>
                     <li><DateOfBirthContainer /></li>
                     <li><KidsContainer /></li>
@@ -91,18 +92,10 @@ class GenderShow extends Component {
 
             <div>
                 <div className="right">
-                    <img src={icon_female} alt="male" />
+                    <img src={icon_female} alt="male" onClick={e => this.props.dispatch({ type: SELECT_GENDER, value: true })} />
                 </div>
-
-                <div className="right-swith">
-                    <label className="switch">
-                        <input type="checkbox" checked = {this.props.isMale} onChange={e => this.props.dispatch({ type: SELECT_GENDER, value: e.target.checked })} />
-                        <span className="slider round"></span>
-                    </label>
-                </div>
-
                 <div className="right">
-                    <img src={icon_male} alt="male" />
+                    <img src={icon_male} alt="male" onClick={e => this.props.dispatch({ type: SELECT_GENDER, value: false })} />
                 </div>
             </div>
         );
@@ -111,7 +104,7 @@ class GenderShow extends Component {
 
 const Smoking = HOC(connect()(SmokingShow), icon_smoking, "smoking");
 
-const Gender = HOC(connect()(GenderShow), icon_smoking, "gender");
+const Gender = HOC(connect()(GenderShow), icon_select_gender, "gender");
 
 
 
