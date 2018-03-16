a094c7b0c12a63a8f0726234bcfd04fa2fe4e1d2
The Hien <thehienpv@gmail.com>
2018-03-16 17:25:04 +0700

    fix dob value

--- my-app/src/components/DateOfBirth.js
+++ my-app/src/components/DateOfBirth.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import dobIcon from "../assets/img/icons-demo/icon_dob.png";
 
 const DateOfBirth = (props) => {
-    const selectedStatus = props.selectedStatus;
+    const selectedDob = props.selectedDob;
     return (
-        <div className="col-md-8" style={{marginTop:'15px'}}>
+        <div className="col-md-8" style={{ marginTop: '15px' }}>
             <input type="date" onChange={e => {
                 props.onSelectDob(e.target.value)
                 props.onClickToShowDatePicker(false)
-            }} />
+            }} className="form-control" value={selectedDob} />
         </div>
     )
 }
